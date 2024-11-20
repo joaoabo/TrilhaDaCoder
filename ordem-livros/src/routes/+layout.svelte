1f648a083bd151d9afa6433f29c2a555b81e6c67
joaoabo <82650085+joaoabo@users.noreply.github.com>
2024-11-20 13:04:38 -0300

    Criado a pagina home e estruturado o projeto

--- ordem-livros/src/routes/+layout.svelte
+++ ordem-livros/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+  import Pagina from "../components/template/Pagina.svelte";
+  let { children } = $props();
 </script>
 
-{@render children()}
+<Pagina>
+  {@render children()}
+</Pagina>
--- ordem-livros/src/routes/+page.svelte
+++ ordem-livros/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold">Ordem dos livros</h1>
+<a href="/autores">Autores</a>
